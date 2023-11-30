dcb7d65e25b4ddd1f8b805251dfdc9111ca7749c
nahyeon99 <nahyeonee99@gmail.com>
2023-11-30 18:29:20 +0900

    feat(controller/BaseballGameController): 컨트롤러 기능 추가

---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,82 @@
+package baseball.controller;
+
+import baseball.domain.BaseballNumbers;
+import baseball.domain.Computer;
+import baseball.domain.HintResult;
+import baseball.domain.RestartStatus;
+import baseball.service.HintService;
+import baseball.util.NumbersFactory;
+import baseball.view.InputView;
+import baseball.view.MessageView;
+import baseball.view.ResultView;
+
+public class BaseballGameController {
+    private final InputView inputView;
+    private final MessageView messageView;
+    private final ResultView resultView;
+    private final HintService hintService;
+    private final Computer computer;
+
+    public BaseballGameController(InputView inputView,
+                                  MessageView messageView,
+                                  ResultView resultView,
+                                  HintService hintService,
+                                  Computer computer) {
+        this.inputView = inputView;
+        this.messageView = messageView;
+        this.resultView = resultView;
+        this.hintService = hintService;
+        this.computer = computer;
+    }
+
+    public void run() {
+        welcome();
+
+        do {
+            play();
+        } while (isRestart());
+    }
+
+    private void welcome() {
+        messageView.printWelcomeMessage();
+    }
+
+    private void play() {
+        while (true) {
+            BaseballNumbers computerNumbers = computer.createRandomNumbers();
+            HintResult hintResult = playOnce(computerNumbers);
+
+            if (is3Strike(hintResult)) {
+                break;
+            }
+        }
+    }
+
+    private HintResult playOnce(BaseballNumbers computerNumbers) {
+        BaseballNumbers userNumbers = NumbersFactory.of(inputView.inputBaseballNumbers());
+
+        HintResult compareResult = hintService.compare(computerNumbers, userNumbers);
+        printHint(compareResult);
+
+        return compareResult;
+    }
+
+    private boolean isRestart() {
+        RestartStatus restartStatus = RestartStatus.valueOf(inputView.inputRestartStatus());
+
+        return restartStatus.equals(RestartStatus.RESTART);
+    }
+
+    private boolean is3Strike(HintResult hintResult) {
+        return hintResult.is3Strike();
+    }
+
+    private void printHint(HintResult hintResult) {
+        if (hintResult.isNothing()) {
+            resultView.printResultOfNothing();
+            return;
+        }
+
+        resultView.printResultOfBallOrStrike(hintResult.getResult());
+    }
+}
--- src/main/java/baseball/exception/ExceptionMessage.java
+++ src/main/java/baseball/exception/ExceptionMessage.java
@@ -5,6 +5,7 @@ public enum ExceptionMessage {
     INVALID_NUMBER_FORMAT("정수로 입력해 주세요."),
     INVALID_NUMBER_RANGE("숫자는 1~9 범위 내에서 입력해 주세요."),
     INVALID_DUPLICATE_NUMBER("숫자 중복 없이 입력해 주세요."),
+    INVALID_RESTART_STATUS("재시작 여부는 1 또는 2로 입력해 주세요."),
     ;
 
     private final String korean;
