1d8c4f25f91293fb2a4955d15d11b9473b10c0be
nahyeon99 <nahyeonee99@gmail.com>
2023-11-30 17:05:12 +0900

    feat(domain/BaseballNumber): 숫자 야구의 숫자 한 개 도메인 추가

---
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -0,0 +1,48 @@
+package baseball.domain;
+
+import static baseball.exception.ExceptionMessage.INVALID_NUMBER_RANGE;
+
+import java.util.Objects;
+
+public class BaseballNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    private final int number;
+
+    private BaseballNumber(int number) {
+        this.number = number;
+    }
+
+    public static BaseballNumber computerFrom(int number) {
+        return new BaseballNumber(number);
+    }
+
+    public static BaseballNumber userFrom(int number) {
+        validateNumberRange(number);
+        return new BaseballNumber(number);
+    }
+
+    private static void validateNumberRange(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE.getKorean());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseballNumber that = (BaseballNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
